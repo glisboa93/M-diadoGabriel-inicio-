655bc083098d9ae15559e02ac67f2f7a336809c8
glisboa93 <96427731+glisboa93@users.noreply.github.com>
2022-03-14 23:47:54 -0300

    Add files via upload
    Primeiro commit.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!"); 
+//tipo de dado + nome da variável  (sintaxe)
+int n1, n2, n3, media;
+//entrada de dados
+Console.WriteLine("Digite a primeira nota: ");
+n1= int.Parse(Console.ReadLine());
+Console.ReadKey();
+Console.WriteLine("Digite a segunda nota: ");
+n2= int.Parse(Console.ReadLine());
+Console.ReadKey();
+Console.WriteLine("Digite a terceira nota: ");
+n3= int.Parse(Console.ReadLine());
+Console.ReadKey();
+
+// processamento
+media= (n1 + n2 + n3) /3;
+Console.WriteLine("A média é: " + media);
+
